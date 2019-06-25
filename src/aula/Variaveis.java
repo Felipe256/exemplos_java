9819dceaa9db76ddb2fda89ed9feb1d7f6ac76b3
Felipe256 <fcavalcante2013.fc@gmail.com>
2019-06-25 13:18:05 -0300

    Exerc2

--- src/aula/Variaveis.java
+++ src/aula/Variaveis.java
@@ -1,17 +1,16 @@
 package aula;
 
+import javax.swing.JOptionPane;
+
 public class Variaveis {
 
 	public static void main(String[] args) {
-		char a = 'a';
-		int i = 0;
-		float f = 10.3f;
-		double d = 10;
+		int i = Integer.parseInt(JOptionPane.showInputDialog("Digite um número"));
+		double d = Double.parseDouble(JOptionPane.showInputDialog("Digite um número"));
+;
 		
 		//TODO Auto-generated method stub
-		System.out.println("a = " + a);
 		System.out.println("i = " + i);
-		System.out.println("f = " + f);
 		System.out.println("d = " + d);
 
 	}
